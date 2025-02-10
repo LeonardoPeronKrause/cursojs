788e22468b7ea72ac86ae7f0678620cdfd6f6f01
LeonardoPeronKrause <lperonkrause@gmail.com>
2025-02-10 14:16:13 -0300

    Estudando

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-showPrimes(10);
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function() {
+        console.log('draw')
+    }
+};
 
-function showPrimes(limit) {
-    for (let number =2; number <= limit; number++)
-     if (isPrime(number)) console.log(number);
+// FACTORY FUNCTION
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('draw')
+        }
+    };
 }
 
-function isPrime(number) {
-    for (let factor = 2; factor < number; factor++) 
-        if (number % factor === 0) 
-            return false;
-        
-    return true;
-}
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
